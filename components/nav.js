71d1c89884d0a674ec596a1e973cae43a51e6c28
Troy Beck <beginnerdev28@gmail.com>
2025-11-08 15:45:08 -0600

    spa capstone

---
+++ components/nav.js
@@ -0,0 +1,14 @@
+import html from "html-literal";
+
+export default () => html `
+    <nav>
+
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Watch List</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="contact.html">Contact US</a></li>
+        </ul>
+
+    </nav>
+`
---
+++ store/about.js
@@ -0,0 +1,5 @@
+
+export default {
+  header: 'About Mat Finder',
+  views: 'about'
+}
